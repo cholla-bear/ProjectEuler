a78c2285d035ed8b5b426d6699ebc8abecba4cc5
cholla-bear <tifojo@gmail.com>
2020-04-23 21:34:28 +0000

    try again

--- problem_19.py
+++ problem_19.py
@@ -1,4 +1,3 @@
-months_30d = [4, 6, 9, 11]
 
 def calendar():
   date = {'year': 1900, 'month': 1, 'day': 1, 'day_of_week': 1}
@@ -7,4 +6,5 @@ def calendar():
     date = next_date(date)
 
 def next_date(date):
+  months_30d = [4, 6, 9, 11]
   pass
