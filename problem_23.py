179a50f043baa5487b471f5d0e997d353f826b48
Tim <tifojo@yahoo.com>
2020-04-23 21:23:32 -0700

    problem 23 working

--- problem_23.py
+++ problem_23.py
@@ -7,13 +7,14 @@ def is_abundant(n):
 
 abundant_nums = list(filter(is_abundant, range(2,28123)))
 
-sum_abundant = np.zeros(28123, dtype=bool)
+sum_abundant = np.zeros(28124, dtype=bool)
 
 for i, a1 in enumerate(abundant_nums):
   for j in range(i, len(abundant_nums)):
     a2 = abundant_nums[j]
     if a1 + a2 < sum_abundant.shape[0]:
       sum_abundant[a1 + a2] = True
+
 
 s = 0
 for n, is_sum in enumerate(sum_abundant):
