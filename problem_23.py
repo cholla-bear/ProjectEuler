706614d1379e748f95097a60a1e93118470d4447
cholla-bear <tifojo@gmail.com>
2020-04-24 04:04:59 +0000

    first crack at 23

--- problem_23.py
+++ problem_23.py
@@ -1,4 +1,5 @@
 from lib import prime
+import numpy as np
 
 def is_abundant(n):
   divisors = prime.proper_divisors(n)
@@ -6,3 +7,11 @@ def is_abundant(n):
 
 abundant_nums = list(filter(is_abundant, range(2,28123)))
 
+sum_abundant = np.zeros(28123, dtype=bool)
+
+for i, a1 in enumerate(abundant_nums):
+  for j in range(i, len(abundant_nums)):
+    a2 = abundant_nums[j]
+    sum_abundant[a1 + a2] = True
+
+print(sum_abundant[:30])
