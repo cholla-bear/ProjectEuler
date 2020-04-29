aabc302c1346d78dbe5592a8711d82248e70b963
cholla-bear <tifojo@gmail.com>
2020-04-29 03:06:36 +0000

    clean up p31 (still bad algorithm)

--- problem_31.py
+++ problem_31.py
@@ -1,22 +1,17 @@
 coin_values = [1, 2, 5, 10, 20, 50, 100, 200]
 
-found_ways = 0
-
-def fill(value, available_coins, coins_so_far):
+def fill(value, available_coins):
   if len(available_coins) == 0:
-    return
+    return 0
   coin_value = available_coins[0]
   max_count = int(value/coin_value)
+  found_ways = 0
   for n in range(max_count + 1):
-    new_coins = coins_so_far.copy()
-    new_coins[coin_value] = n
-    remaining_value = value - n * coin_value
+    remaining_value = value - n*coin_value
     if remaining_value == 0:
-      global found_ways
       found_ways += 1
     else:
-      fill(remaining_value, available_coins[1:], new_coins)
+      found_ways += fill(remaining_value, available_coins[1:])
+  return found_ways
 
-fill(200, list(reversed(coin_values)), {})
-
-print(found_ways)
+print(fill(200, list(reversed(coin_values))))
