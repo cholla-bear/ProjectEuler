e2ebe84173b957e6946477701621ae55d9629ab7
Tim <tifojo@yahoo.com>
2020-05-05 17:47:00 -0700

    p_41 faster

--- problem_41.py
+++ problem_41.py
@@ -1,18 +1,33 @@
-from lib.prime import primes
+from lib.prime import primes, is_prime
 from itertools import takewhile
+import itertools
+from functools import reduce
+from operator import concat
 
 def is_pandigital(n):
   n_str = str(n)
   num_digits = len(n_str)
   if num_digits > 9:
     return False
-  pandigital = set(n_str) == set([str(i) for i in range(1, num_digits + 1)])
-  if pandigital:
-    print(n)
-  return pandigital
+  return set(n_str) == set([str(i) for i in range(1, num_digits + 1)])
 
-upper_bound = 987654321
+def solve_brutal():
+  # Too slow
 
-pandigital_primes = list(filter(is_pandigital, takewhile(lambda x: x <= upper_bound, primes())))
 
-print(pandigital_primes[-1])
+  upper_bound = 987654321
+
+  pandigital_primes = list(filter(is_pandigital, takewhile(lambda x: x <= upper_bound, primes())))
+
+  print(pandigital_primes[-1])
+
+def solve_with_permutations():
+  for num_digits in range(9,0,-1):
+    digits = [str(i) for i in range(num_digits, 0, -1)]
+    permutations = itertools.permutations(digits)
+    for permutation in permutations:
+      n = int(reduce(concat, permutation))
+      if is_prime(n):
+        return n
+
+print(solve_with_permutations())
