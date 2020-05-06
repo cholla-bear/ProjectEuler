8f5cb50518a3696f38f8d79fb68369fad89d3d50
cholla-bear <tifojo@gmail.com>
2020-05-06 00:10:05 +0000

    faster p_40

--- problem_23.py
+++ problem_23.py
@@ -1,24 +1,24 @@
-from lib import prime
-import numpy as np
+# from lib import prime
+# import numpy as np
 
-def is_abundant(n):
-  divisors = prime.proper_divisors(n)
-  return sum(divisors) > n
+# def is_abundant(n):
+#   divisors = prime.proper_divisors(n)
+#   return sum(divisors) > n
 
-abundant_nums = list(filter(is_abundant, range(2,28123)))
+# abundant_nums = list(filter(is_abundant, range(2,28123)))
 
-sum_abundant = np.zeros(28124, dtype=bool)
+# sum_abundant = np.zeros(28124, dtype=bool)
 
-for i, a1 in enumerate(abundant_nums):
-  for j in range(i, len(abundant_nums)):
-    a2 = abundant_nums[j]
-    if a1 + a2 < sum_abundant.shape[0]:
-      sum_abundant[a1 + a2] = True
+# for i, a1 in enumerate(abundant_nums):
+#   for j in range(i, len(abundant_nums)):
+#     a2 = abundant_nums[j]
+#     if a1 + a2 < sum_abundant.shape[0]:
+#       sum_abundant[a1 + a2] = True
 
 
-s = 0
-for n, is_sum in enumerate(sum_abundant):
-  if not is_sum:
-    s += n
+# s = 0
+# for n, is_sum in enumerate(sum_abundant):
+#   if not is_sum:
+#     s += n
 
-print(s)
+# print(s)
--- problem_40.py
+++ problem_40.py
@@ -1,32 +1,51 @@
 from operator import mul
 from functools import reduce
-from itertools import count, islice
-
-def champ(n):
-  '''Return at least n digits of the fractional part of the decimal
-  Champernowe constant as a string'''
-  res = ""
-  i = 1
-  while len(res) < n:
-    res += str(i)
-    i += 1
-  return res
+from itertools import count, islice, takewhile
 
 max_order = 6
 
-champ_str = champ(10**max_order)
+def solve_by_string():
+  def champ(n):
+    '''Return at least n digits of the fractional part of the decimal
+    Champernowe constant as a string'''
+    res = ""
+    i = 1
+    while len(res) < n:
+      res += str(i)
+      i += 1
+    return res
 
-prod = reduce(mul, ([int(champ_str[10**i - 1]) for i in range(max_order + 1)]))
+  max_order = 6
 
-print(prod)
+  champ_str = champ(10**max_order)
 
-def champ_gen():
-  for i in count(1):
-    for digit in str(i):
-      yield int(digit)
+  prod = reduce(mul, ([int(champ_str[10**i - 1]) for i in range(max_order + 1)]))
 
-desired_indices = [10**n - 1 for n in range(max_order + 1)]
-champ_digits = islice(champ_gen(), 10**max_order)
-filtered_digits = filter(lambda x: x[0] in desired_indices, enumerate(champ_digits))
-prod = reduce(mul, (x[1] for x in filtered_digits))
-print(prod)
+  print(prod)
+
+
+def solve_by_generator():
+  def champ_gen():
+    for i in count(1):
+      for digit in str(i):
+        yield int(digit)
+
+  desired_indices = [10**n - 1 for n in range(max_order + 1)]
+  champ_digits = islice(champ_gen(), 10**max_order)
+  filtered_digits = filter(lambda x: x[0] in desired_indices, enumerate(champ_digits))
+  prod = reduce(mul, (x[1] for x in filtered_digits))
+  print(prod)
+
+def champ_digit(n):
+  def digits_up_to_order(j):
+    return sum([9*(i+1)*10**i for i in range(j)])
+  order = 1
+  while digits_up_to_order(order) < n:
+    order += 1
+  index_within_order = n - digits_up_to_order(order - 1)
+  sequence_num, remainder = divmod(index_within_order, order)
+  integer = sequence_num + 10**(order - 1)
+  digit = int(str(integer)[remainder])
+  return digit
+
+print(reduce(mul, [champ_digit(n-1) for n in [10**j for j in range(max_order + 1)]]))
