57399d4acdf076246cab374c8aa32525fbdbb818
cholla-bear <tifojo@gmail.com>
2020-05-06 21:04:56 +0000

    uncomment numpy stuff

--- problem_41.py
+++ problem_41.py
@@ -13,15 +13,12 @@ def is_pandigital(n):
 
 def solve_brutal():
   # Too slow
-
-
   upper_bound = 987654321
-
   pandigital_primes = list(filter(is_pandigital, takewhile(lambda x: x <= upper_bound, primes())))
-
   print(pandigital_primes[-1])
 
 def solve_with_permutations():
+  # Go through all possible pandigital numbers from largest to smallest
   for num_digits in range(9,0,-1):
     digits = [str(i) for i in range(num_digits, 0, -1)]
     permutations = itertools.permutations(digits)
