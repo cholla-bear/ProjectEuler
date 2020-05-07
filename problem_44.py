cfc44b170ef8ad01e84edd7f4406149d6383d34b
Tim <tifojo@yahoo.com>
2020-05-06 19:07:31 -0700

    slightly simpler p_44

--- problem_44.py
+++ problem_44.py
@@ -10,11 +10,11 @@ def pent(n):
   return int(n*(3*n - 1)/2)
 
 def smallest_diff():
-  upper_i = None
   smallest_d = None
   for i in count(1):
-    if upper_i and i > upper_i:
-      break
+    if smallest_d and pent(i) - pent(i - 1) > smallest_d:
+      # we're done, all future diffs greater than smallest_d
+      return smallest_d
     for j in range(i - 1, 0, -1):
       diff = pent(i) - pent(j)
       if smallest_d and diff > smallest_d:
@@ -24,9 +24,6 @@ def smallest_diff():
         continue
       print("found solution: i={} j={} diff={}".format(i, j, diff))
       smallest_d = min(diff, smallest_d) if smallest_d else diff
-      # find the largest index such that pent(i) - pent(i+1) > smallest_d
-      upper_i = int((smallest_d-1)/3) + 1
-      print("upper_i={}".format(upper_i))
   return smallest_d
 
 print(smallest_diff())
