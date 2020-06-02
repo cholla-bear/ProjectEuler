a6d7e7b5621ea33f5af3ee4605f618e4b48a601b
Tim <tifojo@yahoo.com>
2020-06-02 16:11:11 -0700

    comment and clean up p_60

--- problem_60.py
+++ problem_60.py
@@ -7,7 +7,6 @@ max_prime = 10**5
 target_n = 5
 
 primes = sieve(max_prime)
-prime_set = set(primes)
 
 def is_concat_prime(n1, n2):
   return isprime(int(f"{n1}{n2}")) and isprime(int(f"{n2}{n1}"))
@@ -15,18 +14,21 @@ def is_concat_prime(n1, n2):
 def solve_with_graph():
   G = nx.Graph()
   min_sum = None
+
   for i, p1 in enumerate(primes[1:], 1):
-    edges_added = 0
-    if i % 100 == 0:
-      print(f"Tested up to prime: {p1}")
+    # Keep going until no further sums can be smaller than min_sum
     if min_sum and p1 > min_sum:
       return min_sum
+
+    # Populate graph with primes that concatenate with p1
+    edges_added = 0
     for p2 in primes[:i]:
       if is_concat_prime(p1, p2):
         G.add_edge(p1, p2)
         edges_added += 1
+
+    # Hunt for a clique of size target_n
     if edges_added >= target_n - 1:
-      # Hunt for a clique of size target_n by brute force
       neighbors = nx.neighbors(G, p1)
       for candidates in combinations(neighbors, target_n - 1):
         edges = [G.has_edge(*e) for e in combinations(candidates, 2)]
